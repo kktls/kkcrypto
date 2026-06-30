0eba37331504e516a8b836ec1305c9a222abc2db
loncothad <vcs@loncothad.cc>
2026-06-30 16:33:35 +0300

    docs: remove unnecessary line

--- crates/kkcrypto-traits/src/algorithms/rsa.rs
+++ crates/kkcrypto-traits/src/algorithms/rsa.rs
@@ -10,8 +10,7 @@ use crate::{
 /// Defined in [RFC 8017 (PKCS #1 v2.2)](https://datatracker.ietf.org/doc/html/rfc8017).
 pub trait RsaOaep: base::PublicKeyEncryption + markers::Asymmetric + markers::TraditionalMath {}
 
-/// RSA encryption using PKCS#1 v1.5 padding. Required for static RSA key
-/// transport in TLS 1.2.
+/// RSA encryption using PKCS#1 v1.5 padding.
 ///
 /// Defined in [RFC 8017 (PKCS #1 v2.2)](https://datatracker.ietf.org/doc/html/rfc8017).
 pub trait RsaPkcs1v15Enc: base::PublicKeyEncryption + markers::Asymmetric + markers::TraditionalMath {}
